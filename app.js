589c90abbf285a0892d5b96e7b0ea8240e8606bd
omkmorendha <omkmorendha@gmail.com>
2021-12-13 20:38:27 +0530

    Update google auth link

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/resdine",
+  callbackURL: "https://morning-harbor-38050.herokuapp.com:" + port+ "/auth/google/resdine",
   },
   function (accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id, username: profile.emails[0].value, name: profile.displayName, photourl: profile.photos[0].value }, function (err, user) {
