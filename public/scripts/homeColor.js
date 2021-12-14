5304dad3c0798c02f2a8059d35ed74f645defab0
Om Morendha <omkmorendha@gmail.com>
2021-12-14 13:09:48 +0530

    Update homeColor.js

--- public/scripts/homeColor.js
+++ public/scripts/homeColor.js
@@ -1,4 +1,4 @@
-if (window.location.href === "http://localhost:3000/" || window.location.href === "http://localhost:3000/#feature" || window.location.href === "http://localhost:3000/#footer") {
+if (window.location.href === "https://morning-harbor-38050.herokuapp.com/" || window.location.href === "https://morning-harbor-38050.herokuapp.com/#feature" || window.location.href === "https://morning-harbor-38050.herokuapp.com/#footer") {
   $("#home-btn").addClass("_active");
 } else {
   $("#home-btn").removeClass("_active");
